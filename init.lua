8a04f7fd9b815e14d27a0c9b05a1c3728ff9fb0c
Kemal Fadillah <fadillzzz@gmail.com>
2025-03-24 19:57:59 -0700

    Let's fucking goooo

---
+++ init.lua
@@ -0,0 +1,59 @@
+vim.g.base46_cache = vim.fn.stdpath "data" .. "/base46/"
+vim.g.mapleader = " "
+
+-- bootstrap lazy and all plugins
+local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+
+if not vim.uv.fs_stat(lazypath) then
+  local repo = "https://github.com/folke/lazy.nvim.git"
+  vim.fn.system { "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath }
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+local lazy_config = require "configs.lazy"
+
+-- load plugins
+require("lazy").setup({
+  {
+    "NvChad/NvChad",
+    lazy = false,
+    branch = "v2.5",
+    import = "nvchad.plugins",
+  },
+
+  { import = "plugins" },
+}, lazy_config)
+
+-- load theme
+dofile(vim.g.base46_cache .. "defaults")
+dofile(vim.g.base46_cache .. "statusline")
+
+require "options"
+require "nvchad.autocmds"
+
+vim.schedule(function()
+  require "mappings"
+end)
+
+require("overseer").setup()
+require("cmake-tools").setup {}
+require("lspconfig").clangd.setup {
+  on_new_config = function(new_config, _)
+    local status, cmake = pcall(require, "cmake-tools")
+    if status then
+      cmake.clangd_on_new_config(new_config)
+    end
+  end,
+}
+require("conform").setup {
+  formatters_by_ft = {
+    c = { "clang-format" },
+    cpp = { "clang-format" },
+  },
+  format_after_save = {
+    lsp_format = "fallback",
+    async = true,
+  },
+}
+vim.keymap.set("n", "<A-b>", "<cmd> CMakeBuild <cr>", { desc = "Run CMake build" })
---
+++ lua/plugins/init.lua
@@ -0,0 +1,61 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    event = "BufWritePre", -- uncomment for format on save
+    opts = require "configs.conform",
+  },
+
+  -- These are some examples, uncomment them if you want to see them work!
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "configs.lspconfig"
+    end,
+  },
+
+  {
+    "nvim-lua/plenary.nvim",
+    lazy = true,
+  },
+
+  {
+    "stevearc/overseer.nvim",
+    opts = {},
+  },
+
+  {
+    "Civitasv/cmake-tools.nvim",
+    opts = {},
+  },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "c",
+        "cpp",
+      },
+    },
+  },
+
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    config = function()
+      require("copilot").setup {
+        panel = { enabled = false },
+        suggestion = { auto_trigger = true },
+      }
+    end,
+  },
+  -- {
+  -- 	"nvim-treesitter/nvim-treesitter",
+  -- 	opts = {
+  -- 		ensure_installed = {
+  -- 			"vim", "lua", "vimdoc",
+  --      "html", "css"
+  -- 		},
+  -- 	},
+  -- },
+}
