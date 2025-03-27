8b27225e6bbe324bf8f05b9eca1035a636e61f97
Kemal Fadillah <fadillzzz@gmail.com>
2025-03-26 22:50:11 -0700

    Shows full file path in statusline

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,17 +1,42 @@
--- This file needs to have same structure as nvconfig.lua 
+-- This file needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/ui/blob/v3.0/lua/nvconfig.lua
--- Please read that file to know all available options :( 
+-- Please read that file to know all available options :(
+
+local sep_style = "default"
+local utils = require "nvchad.stl.utils"
+
+local sep_icons = utils.separators
+local separators = (type(sep_style) == "table" and sep_style) or sep_icons[sep_style]
+
+local sep_r = separators["right"]
 
 ---@type ChadrcConfig
 local M = {}
 
 M.base46 = {
-	theme = "onedark",
+  theme = "onedark",
 
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
+  -- hl_override = {
+  -- 	Comment = { italic = true },
+  -- 	["@comment"] = { italic = true },
+  -- },
+}
+
+M.ui = {
+  statusline = {
+    modules = {
+      file = function()
+        local x = utils.file()
+
+        if x[2] ~= "Empty" then
+          x[2] = vim.fn.expand "%:p"
+        end
+
+        local name = " " .. x[2] .. (sep_style == "default" and " " or "")
+        return "%#St_file# " .. x[1] .. name .. "%#St_file_sep#" .. sep_r
+      end,
+    },
+  },
 }
 
 -- M.nvdash = { load_on_startup = true }
