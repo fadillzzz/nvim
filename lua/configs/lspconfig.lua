c636cc906ae43974337eee8a537de362da2c993b
fadillzzz <fadillzzz@pm.me>
2025-04-17 00:02:16 -0700

    Updates LSP config to new API

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -3,17 +3,17 @@ require("nvchad.configs.lspconfig").defaults()
 
 local lspconfig = require "lspconfig"
 
--- EXAMPLE
-local servers = { "html", "cssls", "clangd", "ts_ls" }
-local nvlsp = require "nvchad.configs.lspconfig"
+local servers = {
+  html = {},
+  cssls = {},
+  clangd = {},
+  ts_ls = {},
+  rust_analyzer = {},
+}
 
--- lsps with default config
-for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = nvlsp.on_attach,
-    on_init = nvlsp.on_init,
-    capabilities = nvlsp.capabilities,
-  }
+for name, opts in pairs(servers) do
+  vim.lsp.enable(name)
+  vim.lsp.config(name, opts)
 end
 
 -- configuring single server, example: typescript
